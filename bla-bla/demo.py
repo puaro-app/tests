fb910e9abc0de6fbf8b8fccc762050c847041a6a
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-06-16 13:28:08 +0300

    test

--- bla-bla/demo.py
+++ bla-bla/demo.py
@@ -1,4 +1,4 @@
 # WARNING: This method is not secure for production applications.
 # It is shown here for illustrative purposes only.
 
-TEST_PASSWORD = "l7(Vu$mFsddssdffgdsfsdfgdfggsdfsdfgsdfgfdf"
+TEST_PASSWORD = "l7(Vu$mFsddssdffgdsfsdfgdsfsdfgdfggsdfsdfgsdfgfdf"
