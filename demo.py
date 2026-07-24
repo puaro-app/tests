92b99edf4dab3b7cd9f581122a902cedf1fd7420
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-07-24 16:42:50 +0200

    test

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # WARNING: This method is not secure for production applications.
 # It is shown here for illustrative purposes only.
 
-PASSWORD = "l7(Vu$mFsddss3453@*(&$gdsfsdfgghgdfgdfggsdfsdfgsdfgfdf"
+PASSWORD = "l7(Vu$mFsddssdf3453@*(&$"
 data_secret = os.getenv("SECRET")
 
 def make_api_request(endpoint):
