d0c325aefc5ef49f70fc4e5b10a5b5f502004415
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-08-12 11:02:13 +0200

    Update demo.py

--- bla-bla/demo.py
+++ bla-bla/demo.py
@@ -1,4 +1,4 @@
 # WARNING: This method is not secure for production applications.
 # It is shown here for illustrative purposes only.
 
-TEST_PASSWORD = "l7(Vu$mFsddssdfsdfgdsfgfgdsfsdfgdsfsdfgdfggsdfsdfgsdfgfdf"
+TEST_PASSWORD = "l7(Vu$mFsddssdfsd234sdfgsdfgfdf"
