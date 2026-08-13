ab07a59ef8f921a93c963f123bd3ab475f93e524
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-08-13 14:54:08 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@
 PASSWORD = os.environ["SECRET"]
 data_secret = os.getenv("SECRET")
 
-SECRET = "ina52l,S/=jA:_(}v~sdfgsdfgsdfi76(nsB<Q3dv-v]SDb3"
+SECRET = "sdfina52l,S/=jA:_(}v~sdfgsdfgsdfi76(nsB<Q3dv-v]SDb3"
 
 def make_api_request(endpoint):
     """
