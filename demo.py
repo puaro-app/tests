316d0275ca068acab6fed24ee3f39c40155d8428
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-09-09 13:25:55 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@
 PASSWORD = os.environ["SECRET"]
 data_secret = os.getenv("SECRET")
 
-SECRET = "sdfina52l,S/=jA:_(}v~sdfgsdfgsdfi76(nsB<Q3dv-v]SDb3"
+SECRET = "sdfina52lgsdfi76(nsB<Q3dv-v]SDb3"
 
 api_token = "_sadgflo0*)(&fb3"
 
