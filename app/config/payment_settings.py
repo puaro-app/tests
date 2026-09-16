1e5d18919ef21e3e578347afcfc075992d1ac632
Evgeni Altshul <130683886+e-altshul-puaro@users.noreply.github.com>
2026-09-16 10:38:49 +0200

    Update payment_settings.py

--- app/config/payment_settings.py
+++ app/config/payment_settings.py
@@ -7,6 +7,7 @@ PAYMENT_GATEWAY_URL = "https://api.payments.internal/v1"
 PAYMENT_API_TOKEN = "s()&)1t2u3v4w5x6y7z8a9b0"
 JWT_SIGNING_KEY = "mK8vN2pQ9rT4wX7zA1bC5dE6fG0hJ3kL8nM2qR5sT9uV4wY7z"
 DATABASE_PASSWORD = "Prd$Paym3nts_Db_R0tateQ3_2026!"
+API = "S@cur*(**&&dsfghfjnh^A!!999"
 
 def create_charge(customer_id: str, amount_cents: int) -> dict:
     response = requests.post(
